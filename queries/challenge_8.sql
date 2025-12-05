aa6f1186908e3cde5a814783d603b1e5eeea4b88
George <trainee.george.downer@sigmalabs.co.uk>
2025-12-05 15:18:36 +0000

    challenge 8

--- queries/challenge_8.sql
+++ queries/challenge_8.sql
@@ -8,3 +8,22 @@
 
 -- When calculating the order price, ignore any discounts and use the warehouse-standard price for the products only
 
+WITH expensive_prod AS(
+    SELECT unit_price
+    FROM products
+    ORDER BY unit_price DESC
+    LIMIT 1
+),
+total_order AS (
+    SELECT order_id, sum(unit_price*quantity) as order_price
+    FROM order_details
+    GROUP BY order_id)
+
+SELECT order_id,
+        CAST(ROUND(order_price) AS INT) as total_price
+FROM total_order
+WHERE (order_price) > (SELECT unit_price FROM expensive_prod)
+ORDER BY order_id DESC
+LIMIT 10;
+
+        
