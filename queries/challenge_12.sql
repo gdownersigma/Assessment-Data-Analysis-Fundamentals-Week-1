2109616591cac7f8046e40901569a8dc2970d3be
George <trainee.george.downer@sigmalabs.co.uk>
2025-12-05 16:14:43 +0000

    challenge 12

--- queries/challenge_12.sql
+++ queries/challenge_12.sql
@@ -8,3 +8,17 @@
 
 -- All values should be rounded to 2 d.p. for display (but otherwise kept at full precision)
 
+WITH individual_item AS 
+    (SELECT od.order_id,
+            ROUND(CAST(p.unit_price * od.quantity AS NUMERIC), 2) as exp_price,
+            ROUND(CAST(od.unit_price * od.quantity * (1-od.discount) AS NUMERIC), 2) as act_price
+    FROM order_details od
+    JOIN products p ON od.product_id = p.product_id)
+SELECT order_id,
+       SUM(exp_price) as expected_price,
+       SUM(act_price) as actual_price,
+       (SUM(exp_price) - SUM(act_price)) as price_difference
+FROM individual_item
+GROUP BY order_id
+ORDER BY price_difference DESC
+LIMIT 5;
