b98a87dc6ec802c508544071be4b9e220dc016f1
George <trainee.george.downer@sigmalabs.co.uk>
2025-12-05 15:55:30 +0000

    challenge 11

--- queries/challenge_11.sql
+++ queries/challenge_11.sql
@@ -3,4 +3,14 @@
 -- The only column should be 'percentage_discounted'
 
 -- The percentage should be rounded to 2 d.p.
+WITH orders_with_discount AS (
+    SELECT DISTINCT order_id
+    FROM order_details
+    WHERE discount > 0
+)
 
+SELECT ROUND(
+    (CAST((SELECT COUNT(*) FROM orders_with_discount) AS NUMERIC) / 
+     CAST((SELECT COUNT(DISTINCT order_id) FROM order_details) AS NUMERIC)) * 100, 
+    2
+) AS percentage_discounted;
