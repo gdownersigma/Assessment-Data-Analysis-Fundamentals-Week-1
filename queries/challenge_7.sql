ed404a7a68ef3864e594affe6b0390cd233d70a0
George <trainee.george.downer@sigmalabs.co.uk>
2025-12-05 14:46:20 +0000

    challenge_7

--- queries/challenge_7.sql
+++ queries/challenge_7.sql
@@ -2,3 +2,11 @@
 
 -- The column should be called count_of_multiple_orders
 
+WITH mul_orders AS (
+    SELECT order_id, COUNT(*)
+    FROM order_details
+    GROUP BY order_id
+    HAVING COUNT(*) > 1)
+
+SELECT count(*) as count_of_multiple_orders
+FROM mul_orders;
