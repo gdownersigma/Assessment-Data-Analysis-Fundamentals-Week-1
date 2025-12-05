3c878a143f53117b4f3a336fb7646b9478314414
George <trainee.george.downer@sigmalabs.co.uk>
2025-12-05 15:36:55 +0000

    challenge9+10

--- queries/challenge_10.sql
+++ queries/challenge_10.sql
@@ -4,3 +4,17 @@
 
 -- Results should be sorted alphabetically by category.
 
+WITH ranked AS (
+    SELECT 
+    category_name,
+    product_name,
+    unit_price,
+    RANK() OVER (PARTITION BY category_id ORDER BY unit_price DESC) as price_rank
+FROM products
+JOIN categories USING(category_id))
+
+SELECT category_name, product_name
+FROM ranked
+WHERE price_rank = 3
+ORDER BY category_name;
+
